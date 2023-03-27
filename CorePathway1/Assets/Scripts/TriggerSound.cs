8a4aa5f1339fc072ceda23a2d0a4579f79ac1538
reecolaexe <103397814+reecolaexe@users.noreply.github.com>
2023-03-26 20:29:59 -0700

    Support Pathway Build
    This focuses on sound effects using surround sound, audio mixing and script triggers added to the Core Pathway.

--- CorePathway1/Assets/Scripts/TriggerSound.cs
+++ CorePathway1/Assets/Scripts/TriggerSound.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ResetGame : MonoBehaviour
+public class TriggerSound : MonoBehaviour
 {
+    public AudioSource triggerSound;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
-            SceneManager.LoadScene("Test Scene");
+            triggerSound.Play();
         }
     }
 }
